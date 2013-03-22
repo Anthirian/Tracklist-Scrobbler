bbd2c65f5b24de8ed402eb69cb345fff9ffd30e8
Anthirian <gasmelt@gmail.com>
2013-03-22 22:39:17 +0100

    Cleaning up the setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
-try:
-    from distutils.core import setup
-    import py2exe
-    from glob import glob
-    import sys, os
-    import shutil
-    import fnmatch
-    import Parser, Scrobbler
-except ImportError, message:
-    raise SystemExit,  "Unable to load module. %s" % message
+from distutils.core import setup
+import py2exe
+import sys
+import os
+import fnmatch
 
-sys.argv.append('py2exe')
+sys.argv.append("py2exe")
 
 includes = ['pylast']
 options = {'py2exe': {'bundle_files': 3, 'includes': includes, 'optimize': 2}}
 
-def recursive_find_data_files(root_dir, allowed_extensions=tuple('*')):
 
+def recursive_find_data_files(root_dir, allowed_extensions=tuple('*')):
     to_return = {}
     for (dirpath, dirnames, filenames) in os.walk(root_dir):
         if not filenames:
@@ -25,7 +20,7 @@ def recursive_find_data_files(root_dir, allowed_extensions=tuple('*')):
 
             matches_pattern = False
             for cur_pattern in allowed_extensions:
-                if fnmatch.fnmatch(cur_filename, '*.'+cur_pattern):
+                if fnmatch.fnmatch(cur_filename, '*.' + cur_pattern):
                     matches_pattern = True
             if not matches_pattern:
                 continue
@@ -35,21 +30,23 @@ def recursive_find_data_files(root_dir, allowed_extensions=tuple('*')):
 
     return sorted(to_return.items())
 
+
 setup(
-    name = 'Tracklist Scrobbler',
-    version = '1.0',
-    packages = [''],
-    url = '',
-    license = '',
-    author = 'Geert',
-    author_email = '',
-    description = 'Tracklist Scrobbler allows you to scrobble the tracks that were played in a podcast. It parses a tracklist you provide into a last.fm-sanitized format. ',
-    windows = [
+    name='Tracklist Scrobbler',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Geert',
+    author_email='',
+    description='Tracklist Scrobbler',
+    windows=[
         {
             'script': 'Interface.py',
             'icon_resources': [(0, "images\\favicon.ico")]
         }
     ],
-    data_files = recursive_find_data_files('images', ['gif', 'png', 'jpg', 'ico']),
-    options = options
+    data_files=recursive_find_data_files('images', ['gif', 'png', 'jpg', 'ico']),
+    options=options,
+    requires=['pylast', 'py2exe']
 )
