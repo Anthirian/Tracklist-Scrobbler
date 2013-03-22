472a0d270cbbb96b13bfa45c09f776b98de0b35f
Anthirian <gasmelt@gmail.com>
2013-03-22 22:40:03 +0100

    Code cleanup

--- Scrobbler.py
+++ Scrobbler.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 Created on Mar 27, 2012
 
@@ -5,24 +6,28 @@ Created on Mar 27, 2012
 """
 from pylast import md5, LastFMNetwork, WSError
 
+
 class Scrobbler(object):
     """
     classdocs
     """
+
     def __init__(self):
         """
         Constructor
         """
         self.API = "b7b91229e4232d9e6dfa232b3937923e"
         self.secret = "ad646ed84aad9da4f1189454a4872cd9"
-        
+
         # Allow users to create a scrobbler without logging in first
         self.authenticated = False
-    
+
     def login(self, username, password):
         """
         Log in using the username and password provided. Use this function before scrobbling.
         Returns error details in case of failure
+        :param password:
+        :param username:
         """
         password_hash = md5(password)
         try:
@@ -31,10 +36,11 @@ class Scrobbler(object):
             raise
         else:
             self.authenticated = self.lastfm.get_authenticated_user().get_name() == username
-    
+
     def scrobble(self, data):
         """
         Scrobble a list of dicts that each contain a single parsed track to Last.fm
+        :param data: the tracks to scrobble
         """
         if self.authenticated:
             self.lastfm.scrobble_many(data)
